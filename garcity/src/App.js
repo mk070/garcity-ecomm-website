51167b041f164a3fb1e2dae8cf8fdc6c5acd5d93
Madhan <101645880+mk070@users.noreply.github.com>
2024-03-07 13:10:46 +0000

    comit

--- garcity/src/App.js
+++ garcity/src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Nav } from './components/Nav';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <>
       <Nav />
       <Routes>
-        
-        <Route path='/' to/>
+        <Route path='/'  />
       </Routes>
       <Footer />
     </>

8cf950cb39480cccc940f89ef3342a6ce428deb5
Madhan <madhan.king51@gmail.com>
2024-03-09 14:18:44 +0530

    commit

--- garcity/src/App.js
+++ garcity/src/App.js
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Nav } from './components/Nav';
 import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path='/'  />
+        <Route path='/home'  element={<Home />}/>
+        <Route path='/'  element={<Home />}/>
+        <Route path='/home'  element={<Home />}/>
+
       </Routes>
       <Footer />
     </>
--- garcity/src/components/Footer.js
+++ garcity/src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+import MuiLink from '@mui/material/Link';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+       <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <MuiLink color="inherit" href="https://mui.com/">
+        Your Website
+      </MuiLink>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+    </>
   )
 }

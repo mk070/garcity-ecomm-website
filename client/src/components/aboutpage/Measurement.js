90dcf50fdcba65d7d13fb21e46c7d8a5ba24bb3a
naveenrk7 <111naveenkumarrk@gmail.com>
2024-03-28 20:29:13 +0530

    commit

--- client/src/components/aboutpage/Measurement.js
+++ client/src/components/aboutpage/Measurement.js
@@ -1,12 +1,38 @@
 import React, { useState } from 'react';
 import { Container, Grid, Slider, Typography, useMediaQuery, Box, Stack, Button } from '@mui/material';
 import tshirtsVideo from '../../assets/images/contactus/tshirts.mp4'; // Assuming the video path is correct
+import tshirt from '../../assets/images/contactus/tshirt.gif'; // Assuming the video path is correct
 import styled from '@emotion/styled';
 
 const Video = styled.video`
   width: 100%;
   height: 100%;
   object-fit: cover;
+`;
+
+
+const Img = styled.img`
+  width:350px;
+  height: 350px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
 `;
 
 export const Measurement = () => {
@@ -76,6 +102,7 @@ export const Measurement = () => {
           <Grid item>
             <Box bgcolor="#fff" height={400} width={500} display="flex" borderRadius={5} sx={{ boxShadow: '0px 5px 30px -5px rgba(0,0,0,0.8)', flexDirection: 'column', overflow: 'hidden',marginRight:'100px' }}>
               <Video src={tshirtsVideo} controls loop/>
+              {/* <Img src={tshirt} /> */}
             </Box>
           </Grid>
         </Grid>

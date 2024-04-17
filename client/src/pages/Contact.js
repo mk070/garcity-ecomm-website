ba8b3106e82fcaaf1fccc25f2b4b76804ec013b2
Madhan <madhan.king51@gmail.com>
2024-04-17 14:12:28 +0530

    commit

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Maps } from '../components/ContactUs/Maps'
+import  Maps  from '../components/ContactUs/Maps'
 import Fqa from '../components/ContactUs/Fqa'
 import { Hero } from '../components/ContactUs/Hero'
 import InputForm from '../components/ContactUs/InputForm'

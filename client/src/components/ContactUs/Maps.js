300f827ab554ee38f93fd9a747fe7995714acb2a
Madhan <madhan.king51@gmail.com>
2024-04-20 19:35:25 +0530

    commit

--- client/src/components/ContactUs/Maps.js
+++ client/src/components/ContactUs/Maps.js
@@ -3,17 +3,16 @@ import { Box } from '@mui/material';
 
 const Maps = () => {
     return (
-        <Box sx={{ height: {sm:'70vh',xs:'60vh'},padding:{sm:"0 130px",xs:'0 10px'} ,display:"flex", alignItems:"center",marginBottom:"100px"}}
-        >
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1376397.1174207015!2d76.81263792478745!3d10.462393275179181!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9ab59dc85d5b3%3A0x1ddba5a8da46fdc0!2sGARCITY!5e0!3m2!1sen!2sin!4v1713440246421!5m2!1sen!2sin" 
-            width={'100%'} 
-            height={'100%'}
-            allowfullscreen="" 
-            loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade">
+        <Box sx={{ height: {sm:'78vh', xs:'60vh'}, padding:{sm:"0 130px", xs:'0 10px'}, display:"flex", alignItems:"center", marginBottom:"100px"}}>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20801.11726558087!2d77.215651281353!3d11.014696552684017!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9ab59dc85d5b3%3A0x1ddba5a8da46fdc0!2sGARCITY!5e0!3m2!1sen!2sin!4v1713620971481!5m2!1sen!2sin"        width="100%" 
+        height="100%"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</Box>
 
-            </iframe>
-        </Box>
+    
     );
 };
 
--- client/src/components/aboutpage/Hero.js
+++ client/src/components/aboutpage/Hero.js
@@ -13,7 +13,7 @@ export const Hero = () => {
         height:{sm:"65vh", xs:'93vh'},
         marginTop:isSmallScreen?'100px':'140px'}}>
 
-            <Box sx={{  width:isSmallScreen?'80%':'40%', mb:{xs:'20px'}}}> 
+            <Box sx={{  width:isSmallScreen?'100%':'45%', mb:{xs:'20px'}}}> 
                 <img width={'100%'} src={hero} alt="" />
             </Box>
 
@@ -29,7 +29,7 @@ export const Hero = () => {
                 >                   
                     ‘ MAKE YOUR OWN CLOTHING STYLE WITH <span style={{ color: '#DF9573', fontWeight:700, fontFamily:'inherit' }}>  GARCITY ‘</span></Typography>
                     {/* <Typography sx={{marginTop:{sm:'20px' }, paddingRight:{sm:'90px'} ,fontSize:{sm:"24px !important"}, fontWeight:'200px'}}>History:</Typography> */}
-                    <Typography sx={{marginTop:{xs:'30px',sm:'20px' }, paddingRight:{sm:'90px'} ,fontSize:{sm:"18px !important"}}}>
+                    <Typography sx={{marginTop:{xs:'30px',sm:'20px' }, paddingRight:{sm:'150px'} ,fontSize:{sm:"18px !important"}}}>
                         Founded in 2007, GARCITY began as a small garment manufacturing operation in
                         India. Over the years, we've grown into a leader in the industry, with over 15 years of
                         experience delivering top-quality solutions to clients worldwide. <br /> With a track record of

2ad948c840bbe5182f36231d373a02484d911a9f
Madhan <madhan.king51@gmail.com>
2024-05-03 22:30:35 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Enable CORS middleware
 app.use(cors({
-    origin: 'https://garcity.com/', // Adjust the origin based on your frontend URL
+    origin: ['https://garcity.com/','http:localhost:3000','https://www.garcity.com/'], // Adjust the origin based on your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
 }));
